cd9debc129a90a9d7cb1d46f411133600f0f7e1b
Skalwalker <rekanobre@gmail.com>
2018-05-19 13:05:20 -0300

    Integrated with bootstap

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bluePinkTheme.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
