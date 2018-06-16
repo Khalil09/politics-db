63a1283dc09744a0280fbd8d1c156bf6787620b0
claudiosegala <claudiosegalafilho@gmail.com>
2018-06-16 20:44:16 -0300

    passing arguments to component

--- client/src/components/DBTable.vue
+++ client/src/components/DBTable.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="box">
-    <b-table striped hover :items="items"></b-table>
+    <b-table striped hover :items="items">
+      <template slot="show_details" slot-scope="row">
+      <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
+        <b-button size="sm" @click.stop="row.toggleDetails" class="mr-2">
+         {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+        </b-button>
+      </template>
+      <template slot="row-details" slot-scope="row">
+        <b-card>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
+            <b-col>{{ row.item.age }}</b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
+            <b-col>{{ row.item.isActive }}</b-col>
+          </b-row>
+          <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+        </b-card>
+      </template>
+    </b-table>
   </div>
 </template>
 
