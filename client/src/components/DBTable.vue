fe6a290c103dfdd4758ef1ea7cbf6b2e8ca79402
claudiosegala <claudiosegalafilho@gmail.com>
2018-06-16 19:14:57 -0300

    init table

---
+++ client/src/components/DBTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="box">
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  const items = [
+    { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+    { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+    { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+    { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+  ]
+
+  export default {
+    name: 'DBTable',
+    components: {},
+    data () {
+      return {
+        items: items
+      }
+    }
+  }
+</script>
+
+<style>
+  .box {
+    height: 100%;
+    background-color: white;
+  }
+</style>
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <navbar></navbar>
+    <br/>
+    <br/>
+    <b-container fluid>
+      <b-row>
+        <b-col>
+          <!-- put your stuff here -->
+        </b-col>
+        <b-col cols="8">
+          <dbTable></dbTable>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/navbar.vue'
+import DBTable from '@/components/dbtable.vue'
 
 export default {
   name: 'Home',
   components: {
     'navbar': Navbar,
+    'dbTable': DBTable
   },
   data () {
     return {}
