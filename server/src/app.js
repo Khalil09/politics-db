9776bd0d2d75f5e554ac08e7304a3fb2e7a14919
Khalil Carsten <khalilcarsten@gmail.com>
2018-06-17 18:18:25 -0300

    Create dor eleitor endereço

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const indexTable = require('./indexTable')
+const eleitor = require('./eleitor')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/tables', (req, res) => {
   indexTable.getTablesName(req, res);
@@ -19,6 +21,14 @@ app.get('/api/:table', (req, res) => {
 
 app.get('/api/:table/:id', (req, res) => {
   indexTable.getById(req, res);
+})
+
+app.get('/api/eleitor/completo/:id', (req, res) => {
+  eleitor.getEleitor(req, res);
+})
+
+app.post('/api/eleitor/new', (req, res) => {
+  eleitor.createEleitor(req, res);
 })
 
 app.listen(process.env.PORT || 8081)
--- server/src/eleitor.js
+++ server/src/eleitor.js
@@ -3,9 +3,54 @@ const connectDB = require('../lib/connectDB');
 module.exports = {
   getEleitor: function(req, res){
     connectDB.makeConnection((con) => {
-      con.query("select * from " + req.param("table"), (err, row) => {
+      var q = "select eleitor.*, endereco.*, municipio.nome 'municipio', estado.nome 'estado' from eleitor, endereco, municipio, estado where eleitor.id = "
+              + req.param("id") + " AND eleitor.id_endereco = endereco.id"
+              + " AND endereco.id_municipio = municipio.id AND municipio.id_estado = estado.id";
+      con.query(q, (err, row) => {
         if (err) throw err;
         res.json(row);
+      });
+    }, true)
+  },
+
+  createEleitor: function(req, res){
+    var q_end = "INSERT INTO endereco (bairro, id, rua, cep, complemento, id_municipio) VALUES (\"" +
+      req.body.bairro + "\"," +
+      req.body.id + ",\"" +
+      req.body.rua + "\"," +
+      req.body.cep + ",\"" +
+      req.body.complemento + "\"," +
+      req.body.id_municipio + ")";
+
+    var q_elei = "INSERT INTO eleitor (titulo_eleitor, nome, data_de_nasc, genero, id_secao, cep_endereco, id_endereco) VALUES (" +
+      req.body.titulo_eleitor + ",\"" +
+      req.body.nome + "\",\"" +
+      req.body.data_de_nasc + "\",\"" +
+      req.body.genero + "\"," +
+      req.body.id_secao + "," +
+      req.body.cep + "," +
+      req.body.id + ")"
+
+    console.log(q_end);
+
+    connectDB.makeConnection((con) => {
+      con.query(q_end, (err, row) => {
+        if (err) {
+          res.status(400)
+          res.json({"error": "Houve um erro com os campo de enderecos"})
+          throw err;
+        }
+        console.log(q_elei);
+
+        con.query(q_elei, (err, row) => {
+          if (err) {
+            res.status(400);
+            res.json({"error": "Houve um erro com os campo de usuário"})
+            throw err;
+          }
+          res.status(200);
+          res.json({"message": "Criado com sucesso"})
+        });
       });
     }, true)
   }
