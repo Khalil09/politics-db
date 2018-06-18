00c413155889445927b09fefa1d0a1709c646795
Khalil Carsten <khalilcarsten@gmail.com>
2018-06-18 13:42:07 -0300

    changes

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -37,12 +37,7 @@ export default {
     return {
       table: "",
       tables: [],
-      items: [
-        { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-      ],
+      items: [],
       isNotHome: false
     }
   },
