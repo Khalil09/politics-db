a8b18f4d183ff8218f1842d9ecd49b538cf8e4e0
claudiosegala <claudiosegalafilho@gmail.com>
2018-06-18 13:32:51 -0300

    new services

--- client/src/services/TablesService.js
+++ client/src/services/TablesService.js
@@ -4,7 +4,20 @@ export default {
   fetchTables () {
     return Api().get('tables')
   },
-  fetchTableData (table) {
-    return Api().get(table)
+  fetchTableData (table, id) {
+    if (id == null) {
+      return Api().get(table)
+    } else {
+      return Api().get(table + '/' + id)
+    }
+  },
+  createTableData (req, table) {
+    return Api().post(req, table + '/new')
+  },
+  updateTableData (req, table, id) {
+    return Api().post(table + '/update/' + id, req)
+  },
+  removeTableData (table, id) {
+    return Api().delete(table + '/' + id)
   }
 }
