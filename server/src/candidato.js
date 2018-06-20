7d4b05014b265b4a1488ed0fe5077f1f478c572e
Juliana Hosoume <ju.hosoume@gmail.com>
2018-06-19 22:13:44 -0300

    Some fixes in add photo

--- server/src/candidato.js
+++ server/src/candidato.js
@@ -85,39 +85,41 @@ module.exports = {
   },
 
   addPhoto: function(req, res){
-    fs.readFile(`${process.cwd()}/db/fotos/dirceu.jpg`, (err, data) => {
-      //error handle
-      if (err){
-        res.status(500);
-        res.json({"error": "Não foi possível ler a foto"})
-      }
-      //get image file extension name
-      let extensionName = path.extname(`${process.cwd()}/db/fotos/dirceu.png`);
+    connectDB.makeConnection((con) => {
+      fs.readFile(`${process.cwd()}/db/fotos/dirceu.jpg`, (err, data) => {
+        //error handle
+        if (err){
+          res.status(500);
+          res.json({"error": "Não foi possível ler a foto"})
+        }
+        //get image file extension name
+        let extensionName = path.extname(`${process.cwd()}/db/fotos/dirceu.png`);
 
-      //convert image file to base64-encoded string
-      let base64Image = new Buffer(data, 'binary').toString('base64');
+        //convert image file to base64-encoded string
+        let base64Image = new Buffer(data, 'binary').toString('base64');
 
-      //combine all strings
-      let imgSrcString = `data:image/${extensionName.split('.').pop()};base64,${base64Image}`;
+        //combine all strings
+        let imgSrcString = `data:image/${extensionName.split('.').pop()};base64,${base64Image}`;
 
-      connectDB.makeConnection((con) => {
         //send image src string into jade compiler
-        var q = "UPDATE candidato SET candidato.foto = \"" + imgSrcString + "\"' WHERE id = " + req.param("id");;
+        var q = "UPDATE candidato SET candidato.foto = \"" + imgSrcString + "\" WHERE id = " + req.param("
+id");
 
         con.query(q, (err, row) => {
           if (err){
             res.status(400);
             res.json({"error": "Erro ao incluir foto em candidato"})
+            res.json(err)
           }
           res.status(200)
-          if(row.affectedRows == 0){
+          if(row.affectedRows == 0) {
             res.json({"message": "Não existe tal candidato"});
           } else {
             res.json({"message": "Foto incluida com sucesso"});
           }
         });
-      }, true)
-   })
+      })
+    }, true)
   }
 
 }
